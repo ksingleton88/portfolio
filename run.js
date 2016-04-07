28384ad0214d72f91d841d53b04c2f88bd0109a9
chad d <chad@cmfolio.com>
2016-04-07 13:17:35 -0600

    Basic fading in

--- run.js
+++ run.js
@@ -1,6 +1,49 @@
 $(document).ready(function() {
 
-  $('.animated_link_start').addClass('animated_link_end');
+  // - Add an event listener for clicking hamburger menu icon
+  $('.menu-icon').on('click', function() {
+    
+    var $icon = $(this);
+    
+    // If the menu is shown, transitionOut
+    if ($icon.hasClass('menu-open')) {
+      $icon.removeClass('menu-open');
+      transitionOut();
+    }
+    // Else, transitionIn
+    else {
+      $icon.addClass('menu-open');
+      transitionIn();
+    }
+    
+  });
+  
+  function transitionIn() {
+    var time = 0;
+    var delay = 200; // 1000 = 1s
+
+    // Add class to the links
+    $('.animated_link_start').each(function () {
+
+      var $link = $(this);
+      time += delay;
+
+      setTimeout(function() {
+        $link.addClass('animated_link_end');
+      }, time);
+
+    });
+  }
+  
+  function transitionOut() {
+    // - Remove class `animated_link_end` from each link
+    console.log('transitionOut');
+  }
+  
+  // Plugin example
+  // $('.menu-icon').fancyMenu({
+  //   linkClass: '.animated_link_start'
+  // });
   
 });
 
