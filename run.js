fab4b273d02a5ff654dc779d3688372624bd8535
ksingleton88 <ksingleton88@gmail.com>
2016-04-07 10:34:02 -0700

    Initial commit

--- run.js
+++ run.js
@@ -3,3 +3,17 @@
 // 		width: '50%'
 // 	})
 // });
+
+
+// javascript:alert("hello world");
+
+// $(".dropdown").hover(function () {
+//    $(this).find('.button').fadeIn(100);
+//    $(".dropdown-menu").show();
+// },
+
+
+// function () {
+//    $(this).find('.box-hover').fadeOut(100);
+//    $(".box-tresc").hide();
+// });
